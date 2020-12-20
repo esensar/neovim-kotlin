1af48df0c88085fb5cebba3dd3ec97feb66935a0
Ensar Sarajčić <es.ensar@gmail.com>
2020-12-20 23:24:07 +0100

    chore: prepare project for deployment

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,6 +3,7 @@ object Dependencies {
         const val kotlin = "1.4.10"
         const val serialization = "1.0.1"
         const val ktlintGradle = "9.4.1"
+        const val dokkaGradle = "1.4.0"
         const val serializationMsgPack = "0.1.0"
     }
 
--- neovim-api/build.gradle.kts
+++ neovim-api/build.gradle.kts
@@ -1,7 +1,19 @@
 plugins {
     kotlin("multiplatform")
     kotlin("plugin.serialization")
+    id("maven-publish")
+    id("signing")
+    id("org.jetbrains.dokka")
 }
+
+val sonatypeStaging = "https://oss.sonatype.org/service/local/staging/deploy/maven2"
+val sonatypeSnapshots = "https://oss.sonatype.org/content/repositories/snapshots"
+
+val sonatypePassword: String? by project
+val sonatypeUsername: String? by project
+
+val sonatypePasswordEnv: String? = System.getenv()["SONATYPE_PASSWORD"]
+val sonatypeUsernameEnv: String? = System.getenv()["SONATYPE_USERNAME"]
 
 repositories {
     mavenCentral()
@@ -57,5 +69,75 @@ kotlin {
         }
         val nativeMain by getting
         val nativeTest by getting
+    }
+}
+
+tasks {
+    create<Jar>("javadocJar") {
+        dependsOn(dokkaJavadoc)
+        archiveClassifier.set("javadoc")
+        from(dokkaJavadoc.get().outputDirectory)
+    }
+}
+
+signing {
+    isRequired = false
+    sign(publishing.publications)
+}
+
+publishing {
+    publications.withType(MavenPublication::class) {
+        artifact(tasks["javadocJar"])
+        pom {
+            name.set("Neovim Kotlin")
+            description.set("Neovim Kotlin Client API library")
+            url.set("https://github.com/esensar/neovim-kotlin")
+            licenses {
+                license {
+                    name.set("MIT License")
+                    url.set("https://opensource.org/licenses/mit-license.php")
+                }
+            }
+            developers {
+                developer {
+                    id.set("esensar")
+                    name.set("Ensar Sarajčić")
+                    url.set("https://ensarsarajcic.com")
+                    email.set("es.ensar@gmail.com")
+                }
+            }
+            scm {
+                url.set("https://github.com/esensar/neovim-kotlin")
+                connection.set("scm:git:https://github.com/esensar/neovim-kotlin.git")
+                developerConnection.set("scm:git:git@github.com:esensar/neovim-kotlin.git")
+            }
+        }
+    }
+    repositories {
+        maven {
+            url = uri(sonatypeStaging)
+            credentials {
+                username = sonatypeUsername ?: sonatypeUsernameEnv ?: ""
+                password = sonatypePassword ?: sonatypePasswordEnv ?: ""
+            }
+        }
+
+        maven {
+            name = "snapshot"
+            url = uri(sonatypeSnapshots)
+            credentials {
+                username = sonatypeUsername ?: sonatypeUsernameEnv ?: ""
+                password = sonatypePassword ?: sonatypePasswordEnv ?: ""
+            }
+        }
+
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/esensar/neovim-kotlin")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
     }
 }
